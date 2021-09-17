a966fb9cb0ccde08ee1f091d5a6da2992f703a4a
roseghdl <90684967+roseghdl@users.noreply.github.com>
2021-09-17 13:07:01 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("LOVE")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.House)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Chessboard)
+})
+basic.forever(function () {
+	
+})
